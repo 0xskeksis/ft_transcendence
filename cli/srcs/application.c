05c523d36a47bcfe3f59d039149798586add7a56
Eliot Langlois <ellanglo@1D2.42angouleme.fr>
2025-11-26 17:57:48 +0000

    gtk pue sa mere

--- cli/srcs/application.c
+++ cli/srcs/application.c
@@ -6,7 +6,7 @@
 /*   By: ellanglo <ellanglo@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 14:47:04 by ellanglo          #+#    #+#             */
-/*   Updated: 2025/11/26 18:41:25 by ellanglo         ###   ########.fr       */
+/*   Updated: 2025/11/26 17:53:20 by ellanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <Application.h>
@@ -20,7 +20,7 @@ t_App App;
 void create_app()
 {
 	App.curl = curl_easy_init();
-	App.gtk = gtk_application_new("pong.cli", G_APPLICATION_DEFAULT_FLAGS);
+	App.gtk = gtk_application_new("pong.cli", G_APPLICATION_DEFAULT_FLAGS | G_APPLICATION_HANDLES_OPEN);
 	App.Inputs = malloc(sizeof(*App.Inputs));
 	memset(App.Inputs, 0, sizeof(*App.Inputs));
 }
